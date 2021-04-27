be82e0cd965ec464a47ebaaa652a95ad829df4ca
MurrayWarnock <murray.warnock@gmail.com>
2021-04-27 16:39:30 -0600

    Set styling on RegisterForm to match LoginForm - all that needs to be refactored.

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -5,6 +5,50 @@ import * as yup from "yup";
 
 const StyledDiv = styled.div``;
 
+// STYLES COPIED FROM AddRecipe.js - SHOULD BE REFACTORED
+//Styles
+// Had to change flex-direction from PageStyle, so renamed it LoginStyles
+const RegStyle = styled.div`
+  box-sizing: border-box;
+  background-color: #fefae0;
+  width: 100%;
+  border: 1px solid blue;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+const StyledInput = styled.input`
+  width: 15rem;
+  height: 2.5vh;
+  margin: 0.5rem;
+  padding: 2px;
+`;
+const Btn = styled.button`
+  //  display: flex;
+  //  justify-content: center;
+  background-color: #e07a5f;
+  width: 10%;
+  height: 5vh;
+  //  align-content:center;
+  //  align-items: center;
+  // font-size: 1rem;
+  margin: 0.5rem;
+  padding: 2px;
+`;
+
+const ValidationErrs = styled.div`
+  color: red;
+  font-family: sans-serif;
+  font-size: 1rem;
+  font-weight: bold;
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+  margin: 0 auto;
+  justify-content: center;
+  // border: 1px solid red;
+`;
+
 const initialValues = {
   username: "",
   password: "",
@@ -63,38 +107,38 @@ export default function RegisterForm() {
   }, [register]);
 
   return (
-    <StyledDiv>
+    <RegStyle>
       <form onSubmit={submitHandler}>
-        <input
+        <StyledInput
           type="text"
           name="username"
           placeholder="Username"
           onChange={handleChange}
           value={register.username}
         />
-        <input
+        <StyledInput
           type="text"
           name="email"
           placeholder="Email"
           onChange={handleChange}
           value={register.email}
         />
-        <input
+        <StyledInput
           type="password"
           name="password"
           placeholder="Password"
           onChange={handleChange}
           value={register.password}
         />
-        <button disabled={disabled}>Register</button>
+        <Btn disabled={disabled}>Register</Btn>
 
-          <div className="errors">
+          <ValidationErrs className="errors">
             {/* RENDER THE VALIDATION ERRORS HERE */}
             <div>{regFormErrors.username}</div>
             <div>{regFormErrors.email}</div>
             <div>{regFormErrors.password}</div>
-          </div>
+          </ValidationErrs>
       </form>
-    </StyledDiv>
+    </RegStyle>
   );
 }

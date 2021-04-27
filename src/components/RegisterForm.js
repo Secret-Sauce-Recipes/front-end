8d6a4482ef2523fcc65553960c2e44335df12316
MurrayWarnock <murray.warnock@gmail.com>
2021-04-27 16:21:03 -0600

    Yup validation and button behavior added to RegisterForm.js

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import schema from "../validation/login-schema";
+import * as yup from "yup";
 
 const StyledDiv = styled.div``;
 
@@ -9,19 +11,56 @@ const initialValues = {
   email: "",
 };
 
+// Set initial register form error msgs empty
+const initialRegErrors = {
+  username: "",
+  password: "",
+  email: "",
+}
+// set login button initially disabled
+const initialRegDisabled = true;
+
 export default function RegisterForm() {
   const [register, setRegister] = useState(initialValues);
+  const [regFormErrors, setRegFormErrors] = useState(initialRegErrors);
+  const [disabled, setDisabled] = useState(initialRegDisabled);
 
   const handleChange = (e) => {
     setRegister({
       ...register,
       [e.target.name]: e.target.value,
     });
+    const { name, value } = e.target;
+// yup validation
+    yup
+    .reach(schema, name)
+    .validate(value)
+    .then(() => {
+      // happy path and clear the error
+      setRegFormErrors({
+        ...regFormErrors,
+        [name]: "",
+      });
+    })
+    // add error message each time validation unsuccessful
+    .catch((err) => {
+      setRegFormErrors({
+        ...regFormErrors,
+        // validation error from schema
+        [name]: err.errors[0],
+      });
+    });
   };
 
   const submitHandler = (e) => {
     e.preventDefault();
   };
+  useEffect(() => {
+    // ADJUST THE STATUS OF `disabled` EVERY TIME `regFormValues` CHANGES
+    schema.isValid(register).then((valid) => {
+      setDisabled(!valid);
+    });
+  }, [register]);
 
   return (
     <StyledDiv>
@@ -47,7 +86,14 @@ export default function RegisterForm() {
           onChange={handleChange}
           value={register.password}
         />
-        <button>Submit</button>
+        <button disabled={disabled}>Register</button>
+
+          <div className="errors">
+            {/* RENDER THE VALIDATION ERRORS HERE */}
+            <div>{regFormErrors.username}</div>
+            <div>{regFormErrors.email}</div>
+            <div>{regFormErrors.password}</div>
+          </div>
       </form>
     </StyledDiv>
   );
